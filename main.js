743c3bca0ff23c05f28681f8615be3212edcf93b
code-bebop <woonj2@gmail.com>
2022-03-12 17:58:33 +0900

    init
    init commit

---
+++ main.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const log = console.log;
+
+const getHtml = async () => {
+  try {
+    return await axios.get('https://lostark.game.onstove.com/Profile/Character/%EB%AA%A8%EC%BD%94%EC%BD%94%EB%B3%BC%EB%94%B0%EA%B5%AC%EB%B9%A0%EB%8A%94%EC%86%8C%EB%A6%AC');
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 같은 서버의 다른 캐릭터 얻어오기
+const characterAnother = () => {
+  getHtml()
+  .then((html) => {
+    const $ = cheerio.load(html.data);
+
+    const characterServer = $('#expand-character-list > strong').text().replace('@', '');
+    const characterAnotherList = $('#expand-character-list > ul').html();
+
+    log(characterAnotherList);
+
+    return {
+      characterServer
+    };
+  })
+  //  .then((res) => log(res));
+}
+
+characterAnother();
